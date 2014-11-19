54bf8f9df221388b5b8c219f297d822657702879
Paulo Sousa <paulo.sousa@corp.globo.com>
2014-11-19 17:45:18 -0200

    shuffling paths array

--- multiplepaths.lua
+++ multiplepaths.lua
@@ -1,20 +1,34 @@
 counter = 0
 
+-- Initialize the pseudo random number generator - http://lua-users.org/wiki/MathLibraryTutorial
+math.randomseed(os.time())
+math.random(); math.random(); math.random()
+
 function file_exists(file)
   local f = io.open(file, "rb")
   if f then f:close() end
   return f ~= nil
 end
 
+function shuffle(paths)
+  local j, k
+  local n = #paths
+  for i = 1, n do
+    j, k = math.random(n), math.random(n)
+    paths[j], paths[k] = paths[k], paths[j]
+  end
+  return paths
+end
+
 function non_empty_lines_from(file)
   if not file_exists(file) then return {} end
   lines = {}
-  for line in io.lines(file) do 
+  for line in io.lines(file) do
     if not (line == '') then
       lines[#lines + 1] = line
     end
   end
-  return lines
+  return shuffle(lines)
 end
 
 paths = non_empty_lines_from("paths.txt")
@@ -23,7 +37,7 @@ if #paths <= 0 then
   print("multiplepaths: No paths found. You have to create a file paths.txt with one path per line")
   os.exit()
 end
-  
+
 print("multiplepaths: Found " .. #paths .. " paths")
 
 request = function()
